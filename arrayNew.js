4c30681a30397d0776409c42345797bfa0eed80c
Shubhanshu Rastogi <shubhanshu.rastogi@gmail.com>
2025-06-30 20:15:37 +0100

    Add new files

---
+++ arrayNew.js
@@ -0,0 +1,35 @@
+const numbers=[0,1,2,3,4,5];
+
+const fruits=new Array('apple','banana'); 
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(100,200,500))
+
+const userObj= {
+    username: "Shubhanshu",
+    age:42
+}
+
+function handleobject(anyObject){
+    console.log(`username is ${anyObject.username} and age is ${anyObject.age}`)
+}
+
+
+handleobject(userObj)
+
+handleobject({
+      username: "Vibhav",
+    age:40
+})
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
---
+++ srtingex.js
@@ -0,0 +1,5 @@
+//const gameName="Atlantic";
+
+const gameName=new String('Atlantic');
+console.log(gameName[0]);
+//console.log(gameName.__proto__);
